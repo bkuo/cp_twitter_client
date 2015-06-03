475dfc7fda5c19dcefa197414ab16858df184a6c
Bert Kuo <bert@flywheel.com>
2015-06-02 21:01:20 -0700

    tweeters

---
+++ app/src/main/java/com/codepath/apps/mysimpletweets/tweeters/TweeterListActivity.java
@@ -0,0 +1,45 @@
+package com.codepath.apps.mysimpletweets.tweeters;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.codepath.apps.mysimpletweets.R;
+import com.codepath.apps.mysimpletweets.TwitterApplication;
+import com.codepath.apps.mysimpletweets.models.User;
+
+public class TweeterListActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tweeter_list);
+
+        client = TwitterApplication.getRestClient();
+
+        currentUser = (User) getIntent().getSerializableExtra("current_user");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_tweeter_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
